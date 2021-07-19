3bfd3ce7a2a4a4317ec0ebd99ca4943c8f301da6
chongzi <15201061106@163.com>
2021-07-19 14:06:04 +0800

    modify

--- static/sqlfile/MySQL/net_set_56.sql
+++ static/sqlfile/MySQL/net_set_56.sql
@@ -1,3 +1,4 @@
 select VARIABLE_NAME, VARIABLE_VALUE from information_schema.global_variables
 where variable_name in
-('wait_timeout','interactive_timeout','max_allowed_packet','net_read_timeout','net_write_timeout','net_buffer_length','connect_timeout');
+('wait_timeout','interactive_timeout','max_allowed_packet',
+ 'net_read_timeout','net_write_timeout','net_buffer_length','connect_timeout','skip_name_resolve','skip_networking');
