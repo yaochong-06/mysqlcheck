b2066ae67e16ee487a0b2b7a1ee661c45738536e
chongzi <15201061106@163.com>
2021-07-20 21:21:30 +0800

    modify

--- static/sqlfile/MySQL/sessions_res_56.sql
+++ static/sqlfile/MySQL/sessions_res_56.sql
@@ -5,3 +5,5 @@ where Variable_name in ('max_connections','max_user_connections','max_connect_er
 select Variable_name,cast(Variable_Value as signed)
 from performance_schema.global_status
 where Variable_name in ('max_used_connections','Threads_connected','Threads_running');
+
+#
